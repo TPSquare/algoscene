e2d8952099857cf6a19c081343df931973061353
ThanhPhu <132542965+TPSquare@users.noreply.github.com>
2024-06-09 22:25:58 +0700

    THT.1

--- local-data.js
+++ local-data.js
@@ -2,7 +2,7 @@
 
 const localData = new (class {
     constructor() {
-        this.version = '1.0.6';
+        this.version = 'THT.1';
         this.debug = false;
         this.key = 'azlfgvoestczednteu';
         const localData = JSON.parse(localStorage.getItem(this.key)) || {};
