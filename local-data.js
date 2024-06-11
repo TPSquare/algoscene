1ca391f30888572c6b0b07d9227966a6f42277c5
ThanhPhu <132542965+TPSquare@users.noreply.github.com>
2024-06-11 21:56:17 +0700

    v1.0.9

--- local-data.js
+++ local-data.js
@@ -2,7 +2,7 @@
 
 const localData = new (class {
     constructor() {
-        this.version = '1.0.8';
+        this.version = '1.0.9';
         this.debug = false;
         this.key = 'azlfgvoestczednteu';
         const localData = JSON.parse(localStorage.getItem(this.key)) || {};
@@ -43,5 +43,3 @@ const localData = new (class {
 })();
 
 export default localData;
-
-alert('Being continuously updated! / Đang được cập nhật liên tục!')
